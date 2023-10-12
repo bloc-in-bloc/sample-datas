1807d7311a275a5bc0a184243901bb0246fa45c9
Kevin ETOURNEAU <kevin.etourneau@sogelink.com>
2023-10-12 09:09:04 +0200

    Add data

---
+++ OrvaultCC47_Potree1_7_3947/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 2989923,
+    "boundingBox": {
+        "lx": 1351066.535,
+        "ly": 6240248.085,
+        "lz": 49.236000000000007,
+        "ux": 1351300.547,
+        "uy": 6240482.097,
+        "uz": 283.2480000001043
+    },
+    "tightBoundingBox": {
+        "lx": 1351066.535,
+        "ly": 6240248.085,
+        "lz": 49.236000000000007,
+        "ux": 1351236.741,
+        "uy": 6240482.097,
+        "uz": 69.188
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 2.0266034603118898,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
